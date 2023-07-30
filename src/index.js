e1d6a8144cd9e35613a1b5f21cfd84adf6cf33d5
yuliatos12 <125970340+yuliatos12@users.noreply.github.com>
2023-07-30 13:23:50 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -28,17 +28,33 @@ fetchImages(searchQuery, page, perPage).then(data => {
    const results = data.hits;
     gallery.innerHTML = createGalleryMarkUp(results);
 
-    if (data.totalHits <= 1) {
-      
-         loadMoreBtn.classList.add('is-hidden');
+    if (data.totalHits === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+         loadMoreBtn.classList.replace('load-more','is-hidden');
     } else {
+        Notify.info(`Hooray! We found ${data.totalHits} images.`);
         loadMoreBtn.classList.remove('is-hidden');
     }
     
-}).catch(console.warn);
+}).catch(handleError);
+
 
 }
 
+function handleLoadMoreBtnClick () {
+    page += 1;
+    fetchImages(searchQuery, page, perPage).then(data => {
+        const results = data.hits;
+        const numberOfPages = Math.ceil(data.totalHits / perPage);
+        if (numberOfPages === page) {
+            Notify.info("We're sorry, but you've reached the end of search results.");
+            loadMoreBtn.classList.add('is-hidden');
+        }
+         gallery.insertAdjacentHTML('beforeend', createGalleryMarkUp(results));
+        }).catch(handleError);
+
+}
+loadMoreBtn.addEventListener('click', handleLoadMoreBtnClick);
 function createGalleryMarkUp (results) { 
     
     const imgArr = results.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
@@ -46,20 +62,22 @@ function createGalleryMarkUp (results) {
                 <img src="${webformatURL}" alt="${tags}" width="300" loading="lazy" />
         <div class="info">
             <p class="info-item">
-            <b>Likes ${likes}</b>
+            <b>Likes <span>${likes}</span></b>
             </p>
             <p class="info-item">
-            <b>Views ${views}</b>
+            <b>Views <span>${views}</span></b>
             </p>
             <p class="info-item">
-            <b>Comments ${comments}</b>
+            <b>Comments <span>${comments}</span></b>
             </p>
             <p class="info-item">
-            <b>Downloads ${downloads}</b>
+            <b>Downloads <span>${downloads}</span></b>
             </p>
         </div>
         </div>`
     });
     return imgArr.join('');
    };
-   
+   function handleError() {
+    Notify.failure('Something went wrong! Try reloading the page');
+   }
